bc6bad6b7de8479c8b4f4df98e513dd298df5e78
aiko <kiharaaiko72@gmail.com>
2021-04-06 01:54:11 +0000

    laravel 16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -21,7 +21,7 @@ public function create(Request $request)
         $profile = new Profile;
         $form = $request->all();
         
-        
+        unset($profile_form['_token']);
         
         
         $profile->fill($form);
@@ -29,13 +29,41 @@ public function create(Request $request)
         
         return redirect('admin/profile/create');
     }
+    
+     public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts = Profile::where('title', $cond_title)->get();
+    } else {
+        $posts = Profile::all();
+    } 
+    return view('admin.profile.index', ['posts' => $posts, 
+    'cond_title' => $cond_title]);
+    }
 
-public function edit()
+public function edit(Request $request)
 {
-    return view('admin.profile.edit');
+    $profile = Profile::find($request->id);
+        if (empty($profile)){
+            abort(404);
+        }
+    return view('admin.profile.edit', ['profile_form' => $profile]);
 }
-public function update()
-{
-    return redirect('admin/profile/edit');
-}
+
+
+
+public function update(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+        
+        $profile->fill($profile_form)->save();
+        return redirect('admin/profile');
+    }
+    
+   
 }
